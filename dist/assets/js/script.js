e600012043ca1ac448da18683ddf2772a10fbecc
unknown <pakelvi0707@gmail.com>
2021-10-28 12:58:02 +0300

    filter

--- dist/assets/js/script.js
+++ dist/assets/js/script.js
@@ -2,9 +2,31 @@ function test(a, b) {
     return a + b
 }
 
-function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("open");
-  }
+function select1() {
+    document.getElementById("myDropdown1").classList.toggle("open");
+}
+
+  function select2() {
+    document.getElementById("myDropdown2").classList.toggle("open");  
+}
+
+function select3() {
+  document.getElementById("myDropdown3").classList.toggle("open");  
+}
+
+function select4() {
+  document.getElementById("myDropdown4").classList.toggle("open");  
+}
+
+function select5() {
+  document.getElementById("myDropdown5").classList.toggle("open");  
+}
+
+function select6() {
+  document.getElementById("myDropdown6").classList.toggle("open");  
+}
+
+ 
   
 //   function filterFunction() {
 //     var input, filter, ul, li, a, i;
